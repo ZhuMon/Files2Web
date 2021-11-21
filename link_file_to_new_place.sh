b996d7e75acc6fbdaaa7fa7428cd3b5c201ab030
ZhuMon <richardlin0212@gmail.com>
2021-11-21 17:06:04 +0800

    Add ignore file

--- link_file_to_new_place.sh
+++ link_file_to_new_place.sh
@@ -4,4 +4,5 @@ ln css/github-markdown.css $1/css/github-markdown.css
 ln css/github-syntax-highlight.css $1/css/github-syntax-highlight.css
 ln css/mjpage-html.css $1/css/mjpage-html.css
 ln favicon_resized.png $1/favicon_resized.png
+ln ignore.txt $1/ignore.txt
 
