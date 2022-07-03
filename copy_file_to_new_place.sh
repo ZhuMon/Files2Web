b646fe4b6472bc43b2e3f60ccc4adf0b448ad3cd
ZhuMon <richardlin0212@gmail.com>
2022-07-03 20:52:44 +0800

    Rename script
    Change link to copy

---
+++ copy_file_to_new_place.sh
@@ -0,0 +1,8 @@
+cp app.py $1/app.py
+mkdir -f $1/css
+#ln css/github-markdown.css $1/css/github-markdown.css
+#ln css/github-syntax-highlight.css $1/css/github-syntax-highlight.css
+#ln css/mjpage-html.css $1/css/mjpage-html.css
+cp favicon_resized.png $1/favicon_resized.png
+cp -r css $1/
+
--- link_file_to_new_place.sh
+++
@@ -1,7 +0,0 @@
-ln app.py $1/app.py
-mkdir $1/css
-ln css/github-markdown.css $1/css/github-markdown.css
-ln css/github-syntax-highlight.css $1/css/github-syntax-highlight.css
-ln css/mjpage-html.css $1/css/mjpage-html.css
-ln favicon_resized.png $1/favicon_resized.png
-
